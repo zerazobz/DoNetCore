214da3283a383be228dd9b5130ec09e3223b3309
Erick Asto Oblitas <erickastooblitas>
2020-10-04 13:26:55 -0500

    Seed added

---
+++ DotNetCoreExample/DotNetCoreExample/Models/SeedData.cs
@@ -0,0 +1,42 @@
+﻿using DotNetCoreExample.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreExample.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new DotNetCoreExampleContext(serviceProvider.GetRequiredService<DbContextOptions<DotNetCoreExampleContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(new Movie()
+                {
+                    Title = "我不是药神",
+                    Country = "中国",
+                    ReleaseDate = new DateTime(2018, 06, 19),
+                    Genre = "Comedy/Drama",
+                    Price = 10900000.00m
+                },
+                new Movie()
+                {
+                    Title = "Tropa de Elite - Missão Dada é Missão Cumprida",
+                    Country = "Brasil",
+                    ReleaseDate = new DateTime(2017, 5, 10),
+                    Genre = "Policial/Drama",
+                    Price = 10500000.00m
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- DotNetCoreExample/DotNetCoreExample/Program.cs
+++ DotNetCoreExample/DotNetCoreExample/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCoreExample.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +15,22 @@ namespace DotNetCoreExample
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();//.Run();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error has occurred seeding the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
